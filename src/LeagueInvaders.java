297901debf6935f966f85cbd26075179ffb89af0
League Student <league@iMac-18.attlocal.net>
2020-03-03 19:52:59 -0800

    3/3/20
    Finished first part

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,19 +1,27 @@
 import javax.swing.JFrame;
 
 public class LeagueInvaders {
+
+	   
 	private JFrame frame;
 	public static final int WIDTH = 500;
 	public static final int HEIGHT = 800;
-	
+	private GamePanel panel;
 public static void main(String[] args) {
 	
+
+	new LeagueInvaders().setup();
 }
-LeagueInvaders(JFrame frame){
-	frame = this.frame;
+LeagueInvaders(){
+	panel = new GamePanel();
+	frame = new JFrame();
 }
 void setup() {
+	frame.add(panel);
 	frame.setSize(WIDTH, HEIGHT);
 	frame.setVisible(true);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
 }
+
 }
